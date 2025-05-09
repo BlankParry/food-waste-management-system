5d5cf6f6c5996822c3f012e469b53d882d3d8131
BlankParry <kmamogh.ssg@gmail.com>
2025-05-09 12:59:10 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,10 @@
-
-
 <?php
-//change mysqli_connect(host_name,username, password); 
-$connection = mysqli_connect("localhost", "root", "");
-$db = mysqli_select_db($connection, 'demo');
+// Connect to MariaDB/MySQL with your credentials
+$connection = mysqli_connect("localhost", "fwmsuser", "mhray345");
+$db = mysqli_select_db($connection, 'fwmsdb');
+
+// Check connection
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
